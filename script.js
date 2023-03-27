35396f1cba3d9e12cd1bde05b50e3ad998187e74
Ganapathy77 <106886401+Ganapathy77@users.noreply.github.com>
2023-03-27 23:19:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function test() {
+	const promise = new Promise((resolve,reject) => {
+		setTimeOut(()=> {
+			resolve("Hello, world!");
+		}, 1000)
+	})
+}
+
+test().then((data)=>{
+	console.log(data)
+})
